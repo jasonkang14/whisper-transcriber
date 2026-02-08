f8867d4322fb979825e73cacd8247118dd5c9a0d
Jason Kang <jasonkang14@gmail.com>
2026-02-08 22:28:22 +0000

    modify settings to use custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/whisper-transcriber/",
+    base: "/",
     plugins: [react()],
     optimizeDeps: {
         exclude: ["@huggingface/transformers"],
